7cdd841d5304fbed2f76ebcff47391be3ece9807
stevenlidl <stevenlidl@yahoo.com>
2022-03-26 19:48:44 -0500

    update

--- mp6SQL.sql
+++ mp6SQL.sql
@@ -5,6 +5,6 @@ CREATE TABLE superhero (
   hero VARCHAR(30),
   power VARCHAR(30), 
   name VARCHAR(30), 
-  xp, VARCHAR(30) 
-  color VARCHAR(30)
-  PRIMARY KEY(id));;
+  xp, VARCHAR(30),
+  color VARCHAR(30),
+  PRIMARY KEY(id));
