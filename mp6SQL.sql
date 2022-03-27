43fea4079f4f4f0025d69e38171fadf31934e19d
stevenlidl <stevenlidl@yahoo.com>
2022-03-26 19:39:19 -0500

    add

---
+++ mp6SQL.sql
@@ -0,0 +1,10 @@
+CREATE database mp6;
+USE mp6;
+CREATE TABLE superhero (
+  id INT UNSIGNED AUTO_INCREMENT,
+  hero VARCHAR(30),
+  power VARCHAR(30), 
+  name VARCHAR(30), 
+  xp, VARCHAR(30) 
+  color VARCHAR(30)
+  PRIMARY KEY(id));;
